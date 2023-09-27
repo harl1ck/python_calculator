40b0e78252cf9db3458f785c9281a14bed32eb87
harl1ck <anastasia.ovtsunova@gmail.com>
2023-09-27 11:35:42 +0300

    commit

--- main.py
+++ main.py
@@ -1,5 +1,44 @@
 import math
 
+
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def modify(a, b):
+    return a * b
+
+
+def divide(a, b):
+    return a / b
+
+
+def pow(a, b):
+    return a ** b
+
+
+def sqrt(a):
+    return math.sqrt(a)
+
+
+def factorial(a):
+    return math.factorial(int(a))
+
+
+def sin(a):
+    return math.sin(a)
+
+
+def cos(a):
+    return math.cos(a)
+
+
+def tan(a):
+    return math.tan(a)
 z = ""
 try:
     a = float(input("Введите первое число:"))
@@ -7,61 +46,26 @@ try:
 
     if not (z == "sqrt" or z == "!" or z == "sin" or z == "cos" or z == "tan"):
         b = float(input("Введите второе число:"))
-except (ValueError):
+except ValueError:
     print("НЕ ЧИСЛО")
 
-    match z:
-        case "+": print(a + b)
-        case "-": print(a - b)
-        case "*": print(a * b)
-        case "/":
-            if b == 0:
-                print("Я не могу посчитать :_(")
-            else:
-                print(a / b)
-        case "**": print(a ** b)
-        case "sqrt": print(math.sqrt(a))
-        case "!": print(math.factorial(a))
-        case "sin": print(math.sin(a))
-        case "cos": print(math.cos(a))
-        case "tan": print(math.tan(a))
+match z:
+    case "+": print(add(a, b))
+    case "-": print(subtract(a, b))
+    case "*": print(modify(a, b))
+    case "/":
+        if b == 0:
+            print("Я не могу посчитать :_(")
+        else:
+            print(divide(a, b))
+    case "**": print(pow(a, b))
+    case "sqrt": print(sqrt(a))
+    case "!":
+        try:
+            print(factorial(a))
+        except ValueError:
+            print("Число не подходит")
+    case "sin": print(sin(a))
+    case "cos": print(cos(a))
+    case "tan": print(tan(a))
 
-
-    def add(a, b):
-        return a + b
-
-
-    def subtract(a, b):
-        return a - b
-
-
-    def modify(a, b):
-        return a * b
-
-
-    def divide(a, b):
-        return a / b
-
-
-    def pow(a, b):
-        return a ** b
-
-
-    def sqrt(a):
-        return math.sqrt(a)
-
-
-    def factorial(a):
-        return math.factorial(int(a))
-
-
-    def sin(a):
-        return math.sin(a)
-
-
-    def cos(a):
-        return math.cos(a)
-
-    
-    def tan(a):
-        return math.tan(a)
